c6abc5f2875d3983b53dabbf865ea99a4c4a0c54
Alexander <knibz@protonmail.com>
2022-07-10 23:42:29 -0400

    edit doc ready function to omit show errors

--- src/index.js
+++ src/index.js
@@ -2,35 +2,37 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import BitcoinCompanies from './bitcoin';
+import {BitcoinCompanies} from './bitcoin';
 
-function clearFields() {
-  $('#coin').val("");
-}
+// function clearFields() {
+//   $('#coin').val("");
+// }
+
+
 
 $(document).ready(function() {
   $('#bitGet').submit(function(event){
     event.preventDefault();
     let coin = $('#coin').val();
-    clearFields();
-    let promise = BitcoinCompanies.getCoinInfo(coin);
-    promise.then(function(response){
-      const responseData = JSON.parse(response);
-      for  (let i = 0; i < 10; i++) {
-      $('.showCompany').append(`${responseData[i].id}`);
-      }
-    });
+    $('#coin').val("");
+
+    let companies = new BitcoinCompanies();
+
+    $('.showName').text(`${coin} is ${companies}`);
+    $('.showErrors').text("");
+   // $('.showErrors').text(`There was an error processing your request`); 
+    
   });
 });
 
-// function getElements(response) {
-//   if (response.id) {
-//     $('.showCompany').text(`The humidity in ${response.id}`);
+
+// function getElements (response) {
+//   if (response.name) {
+//     $('.showName').text(`The humidity in ${response.name}`);
 //   } else {
 //     $('.showErrors').text(`There was an error: ${response}`);
 //   }
 // }
-
 // async function makeApiCall() {
 //   const response = await BitcoinCompanies.getCoinInfo();
 //   getElements(response);
@@ -48,3 +50,12 @@ $(document).ready(function() {
 
 
 
+// let promise = BitcoinCompanies.getCoinInfo(coin);
+//     promise.then(function(response) {
+//       const responseData = JSON.parse(response);
+//       $('.showNameBtc').append(`${responseData[0].id.name.price}`);
+//       // $('.showNameEth').append(`${responseData[1].name}`);
+//       // $('.showNameDoge').append(`${responseData[9].name}`);
+//       // for  (let i = 0; i < 10; i++) {
+//       //   $('.showName').append(`${responseData[i].name}`);
+//       // }
