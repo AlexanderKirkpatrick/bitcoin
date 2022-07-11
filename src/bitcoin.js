49a797a625a50a4508916067d1fc9d9e1e885a78
Alexander <knibz@protonmail.com>
2022-07-10 23:40:29 -0400

    edit getCoinInfo to be try and catch, with async await functionality

--- src/bitcoin.js
+++ src/bitcoin.js
@@ -1,33 +1,23 @@
-export default class BitcoinCompanies {
-  static getCoinInfo() {
-    return new Promise(function(resolve, reject){
-      let request = new XMLHttpRequest();
-      const url = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
+export class BitcoinCompanies {
+  static async getCoinInfo(userInput) {
+    try {
+      const response = await fetch `https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids=${userInput}&interval=1d,30d&convert=EUR&platform-currency=ETH&per-page=100&page=1`;
+
+      if (!response.ok) {
+        throw Error(response.status);
+      }
+      return response.json();      
+    } catch(error) {
+      return error.message;
+
+    }
   }
 }
 
 
-// export default class BitcoinCompanies {
-//   static async getCoinInfo() {
-//     try {
-//       const response = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&platform-currency=ETH&per-page=100&page=1`);
-//       if(!response.ok) {
-//         throw Error(response.statusText);
-//       }
-//       return response.json(); 
-//     } catch(error) {
-//       return error.message;
-//     }
-//   }
-// }
+
+
+
+
+
 
